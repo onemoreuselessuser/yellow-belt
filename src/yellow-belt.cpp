f171b1e422a3c0ebe92128e32239a793a3b5fdad
Danil Kuznetsov <mail@dkuznetsov.info>
2017-12-25 15:57:42 +0300

    Initial commit

---
+++ src/yellow-belt.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : yellow-belt.cpp
+// Author      : Danil Kuznetsov
+// Version     :
+// Copyright   : --
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
